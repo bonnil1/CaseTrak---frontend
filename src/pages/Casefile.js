899d9ea5a704876e065544ea24fb779488d5338c
bonnil1 <bonnieliu759@yahoo.com>
2024-05-27 21:10:33 -0700

    deploy debug

--- src/pages/Casefile.js
+++ src/pages/Casefile.js
@@ -95,22 +95,22 @@ const Casefile = (props) => {
               <React.Fragment key={index}>
                 <div className="flex flex-col items-center mt-5">
                 <h3>Item Number: </h3>
-                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2'>
+                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2 overflow-scroll'>
                         {ev.number}
                     </h3>
 
                     <h3>Type: </h3>
-                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2'>
+                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2 overflow-scroll'>
                         {ev.type}
                     </h3>
 
                     <h3>Description: </h3>
-                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2'>
+                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2 overflow-scroll'>
                         {ev.description}
                     </h3>
 
                     <h3>Location: </h3>                                
-                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2'>
+                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2 overflow-scroll'>
                         {ev.location}
                     </h3>
                 </div>
@@ -130,22 +130,22 @@ const Casefile = (props) => {
               <React.Fragment key={index}>
                 <div className="flex flex-col items-center mt-5">
                 <h3>Name: </h3>
-                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2'>
+                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2 overflow-scroll'>
                             {inv.name}
                     </h3>
 
                     <h3>Unit: </h3>
-                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2'>
+                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2 overflow-scroll'>
                         {inv.unit}
                     </h3>
 
                     <h3>Contact Number: </h3>
-                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2'>
+                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2 overflow-scroll'>
                         {inv.number}
                     </h3>
 
                     <h3>Email: </h3>
-                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2'>
+                    <h3 className='appearance-none block w-1/3 bg-blue-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-blue-100 mt-2 overflow-scroll'>
                         {inv.email}
                     </h3>
                 </div>
